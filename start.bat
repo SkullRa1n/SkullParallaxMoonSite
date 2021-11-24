343d47271222f48f19ca0d12816ab5b6c8277160
M0rthifxr <chiiobwd@hotmail.com>
2021-11-24 17:21:24 +0000

    Segundo Update

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 git init
 git add *
-git commit -m "Primeiro Update"
+git commit -m "Segundo Update"
 git branch -M main
 git remote add origin https://github.com/SkullRa1n/SkullParallaxMoonSite.git
 git push -u origin main
